85738faa96f00e0196490387e25458053505fc3f
SD13 <SD13@mitt.ca>
2018-05-17 22:56:48 -0500

    Auto-Prefixing of the CSS

---
+++ gulpFile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+
+
+
+
+gulp.task('default', function(){
+
+
+});
+
+
+gulp.task('css', function(){
+    gulp.src('src/**/*.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+
+    .pipe(gulp.dest("./dist/css/"))
+});
