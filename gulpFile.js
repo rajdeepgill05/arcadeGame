415eb4544249b1289423f92d40c72aa90efc4adf
SD13 <SD13@mitt.ca>
2018-05-17 23:04:41 -0500

    Minification of CSS Using Gulp-Clean

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 
 
@@ -18,6 +19,8 @@ gulp.task('css', function(){
         browsers: ['last 2 versions'],
         cascade: false
     }))
+
+    .pipe(cleanCSS({compatibility: 'ie8'}))
 
     .pipe(gulp.dest("./dist/css/"))
 });
